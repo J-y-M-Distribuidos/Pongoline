f7423d596fef65affbd2058ceed821c878dde33a
mainfavin <mainfavin@unirioja.es>
2023-11-18 12:07:12 +0100

    Por fin esta la velocidad de la bola variable, MODOS DE DIFICULTAD?

--- src/Bola.java
+++ src/Bola.java
@@ -2,27 +2,51 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
+import java.lang.Math;
 public class Bola extends Rectangle{
 	Random random;
 	int xVelocity;
 	int yVelocity;
+	int velocidad = 4;
 	
-	public Bola() {
+	public Bola(int x, int y, int diametro) {
+		super(x,y,diametro,diametro);
+		random = new Random();
+		int randXDir = random.nextInt(2);// 0 o 1
+		int randYDir = random.nextInt(2);// 0 o 1
+		if(randXDir == 0) {
+			randXDir --;
+		}
+		setXDirection(randXDir);
 		
+		if(randYDir == 0) {
+			randYDir --;
+		}
+		setYDirection(randYDir);
 	}
 	
 	public void setXDirection(int randXDir) {
-
+		xVelocity = randXDir ;
 	}
 	
 	public void setYDirection(int randYDir) {
-
+		yVelocity = randYDir ;
 	}
 	
+	//x e y sonatributos de la clase padre rectangulo. Se ve mejor con this.x/this.y
 	public void move() {
+		if(Math.abs(xVelocity) != velocidad  || Math.abs(yVelocity) != velocidad) {
+			x += velocidad * xVelocity;
+			y += velocidad * yVelocity;
+		}else {
+			x += xVelocity;
+			y += yVelocity;
+		}
 		
 	}
 	public void draw(Graphics g) {
+		g.setColor(Color.GREEN);
+		g.fillOval(x, y, height, width);
 		
 	}
 }
--- src/PanelJuego.java
+++ src/PanelJuego.java
@@ -9,7 +9,7 @@ public class PanelJuego extends JPanel implements Runnable {
 	static final int GAME_WIDTH = 1000;
 	static final int GAME_HEIGHT = (int) (GAME_WIDTH * (0.5625));
 	static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
-	static final int BALL_DIMAETER = 20;
+	static final int BOLA_DIMAETER = 20;
 	static final int RAQUETA_WIDTH = 20;
 	static final int RAQUETA_HEIGHT = 125;
 	Thread juegoThread;
@@ -35,7 +35,8 @@ public class PanelJuego extends JPanel implements Runnable {
 
 	// Crea una nueva bola en caso de que queramos resetear la actual.
 	public void newBola() {
-
+		random = new Random();
+		bola = new Bola((GAME_WIDTH / 2) - (BOLA_DIMAETER / 2), (GAME_HEIGHT / 2) - (BOLA_DIMAETER / 2), BOLA_DIMAETER);
 	}
 
 	// Crea nuevas palas para los jugadores en caso de que queramos resetear las
@@ -55,39 +56,61 @@ public class PanelJuego extends JPanel implements Runnable {
 		g.drawImage(image, 0, 0, this);
 	}
 
+	// dibuja en pantalla
 	public void draw(Graphics g) {
 		pala1.draw(g);
 		pala2.draw(g);
+		bola.draw(g);
 	}
 
-	// Mueve cosas
+	// Vamos a hacer que el movimiento de las palas no sea caca.
+	// Asi se mueven tanto con el evento como con el move de raqueta --> dos inputs
+	// de movimiento.
 	public void move() {
-
+		pala1.move();
+		pala2.move();
+		bola.move();
 	}
 
 	// Compureba la colisión de los elementos del juego
 	// Previene que las palas se salgan de la pantalla.
 	public void checkColision() {
-		//Axis X
+		// Axis X
 		if (pala1.x <= 0)
 			pala1.x = 0;
 		if (pala1.x >= (GAME_WIDTH - RAQUETA_WIDTH))
 			pala1.x = GAME_WIDTH - RAQUETA_WIDTH;
-		
+
 		if (pala2.x <= 0)
 			pala2.x = 0;
-		if (pala2.x >= (GAME_WIDTH- RAQUETA_WIDTH))
+		if (pala2.x >= (GAME_WIDTH - RAQUETA_WIDTH))
 			pala2.x = GAME_WIDTH - RAQUETA_WIDTH;
-		//Axis Y
+		if (bola.x <= 0) {
+			bola.x = 0;
+			bola.setXDirection(-bola.xVelocity);
+		}
+		if (bola.x>= (GAME_WIDTH - BOLA_DIMAETER)) {
+			bola.x = GAME_WIDTH-BOLA_DIMAETER;
+			bola.setXDirection(-bola.xVelocity);
+		}
+		// Axis Y
 		if (pala1.y <= 0)
 			pala1.y = 0;
 		if (pala1.y >= (GAME_HEIGHT - RAQUETA_HEIGHT))
 			pala1.y = GAME_HEIGHT - RAQUETA_HEIGHT;
-		
+
 		if (pala2.y <= 0)
 			pala2.y = 0;
 		if (pala2.y >= (GAME_HEIGHT - RAQUETA_HEIGHT))
 			pala2.y = GAME_HEIGHT - RAQUETA_HEIGHT;
+		if (bola.y <= 0) {
+			bola.y = 0;
+			bola.setYDirection(-bola.yVelocity);
+		}
+		if (bola.y>= (GAME_HEIGHT - BOLA_DIMAETER)) {
+			bola.y = GAME_HEIGHT-BOLA_DIMAETER;
+			bola.setYDirection(-bola.yVelocity);
+		}
 	}
 
 	// Runea el juego
