6e77ee1036e4539d277e9e8a8af101ed83534562
mainfavin <mainfavin@unirioja.es>
2023-12-09 12:18:35 +0100

    ojo que te cojo, que tenemos un robot que simula la pulsacion de las teclas del jugador que nos manda datos
    eso

--- src/red/RecibirDatosJuego.java
+++ src/red/RecibirDatosJuego.java
@@ -1,10 +1,12 @@
 package red;
 
+import java.awt.AWTException;
+import java.awt.Robot;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.*;
 
-//Esta es la clase donde recibimos el movimiento de la pala del cliente.
+//Esta es la clase donde recibimos el movimiento de la pala del cliente y simulamos sus pulsaciones.
 public class RecibirDatosJuego implements Runnable {
 	private Socket socket;
 	private byte datos;
@@ -15,14 +17,18 @@ public class RecibirDatosJuego implements Runnable {
 
 	@Override
 	public void run() {
-		
-		try (DataInputStream din = new DataInputStream(socket.getInputStream());){ //aqui
+
+		try (DataInputStream din = new DataInputStream(socket.getInputStream());) { // aqui
+			Robot robot = new Robot();
 			while (true) {
 				datos = din.readByte();
+				robot.keyPress(datos);
+				robot.keyRelease(datos);
 				System.out.println(datos);
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AWTException e) {	
 			e.printStackTrace();
 		}
 
